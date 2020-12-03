574019c2a7b7f9e8e16bb71c04e05647261f6667
Steven Nunez <octosteve@github.com>
2020-12-02 22:19:45 -0500

    Extract Container

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -13,18 +13,11 @@ defmodule Bottles do
       "no more bottles of beer on the wall.\n"
   end
 
-  def verse(2) do
-    "2 bottles of beer on the wall, " <>
-      "2 bottles of beer.\n" <>
-      "Take one down and pass it around, " <>
-      "1 bottle of beer on the wall.\n"
-  end
-
   def verse(verse_number) do
-    "#{verse_number} bottles of beer on the wall, " <>
+    "#{verse_number} #{container(verse_number)} of beer on the wall, " <>
       "#{verse_number} bottles of beer.\n" <>
       "Take one down and pass it around, " <>
-      "#{verse_number - 1} bottles of beer on the wall.\n"
+      "#{verse_number - 1} #{container(verse_number - 1)} of beer on the wall.\n"
   end
 
   def verses(start_verse, end_verse) do
@@ -35,5 +28,13 @@ defmodule Bottles do
 
   def song do
     verses(99, 0)
+  end
+
+  defp container(1) do
+    "bottle"
+  end
+
+  defp container(_number) do
+    "bottles"
   end
 end
