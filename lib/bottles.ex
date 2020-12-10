b53ffc648084b6d6bac198864a406cf7c8f66a56
Steven Nunez <octosteve@github.com>
2020-12-10 16:26:31 -0500

    Extract Concepts

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -18,40 +18,46 @@ defmodule Bottles do
     verses(99, 0)
   end
 
-  defp container(1) do
-    "bottle"
-  end
-
-  defp container(_number) do
-    "bottles"
-  end
+  # ---------------------
 
   defp quantity(0) do
     "no more"
-  end
-
-  defp quantity(number) do
-    number
-  end
-
-  defp pronoun(1) do
-    "it"
-  end
-
-  defp pronoun(_) do
-    "one"
   end
 
   defp action(0) do
     "Go to the store and buy some more, "
   end
 
-  defp action(verse_number) do
-    "Take #{pronoun(verse_number)} down and pass it around, "
-  end
-
   defp successor(0) do
     99
+  end
+
+  # ---------------------
+
+  defp container(1) do
+    "bottle"
+  end
+
+  defp pronoun(1) do
+    "it"
+  end
+
+  # ---------------------
+
+  defp container(_number) do
+    "bottles"
+  end
+
+  defp quantity(number) do
+    number
+  end
+
+  defp pronoun(_) do
+    "one"
+  end
+
+  defp action(verse_number) do
+    "Take #{pronoun(verse_number)} down and pass it around, "
   end
 
   defp successor(verse_number) do
