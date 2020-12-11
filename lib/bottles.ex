6b46d452362ef57b9b9f43793b16ff944230306b
Steven Nunez <octosteve@github.com>
2020-12-11 13:07:48 -0500

    Update with Bruce notes

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -16,30 +16,25 @@ defmodule BottleNumber do
   def successor(%__MODULE__{successor: successor}), do: new(successor)
 
   def apply_defaults(%__MODULE__{number: number} = struct) do
-    struct
-    |> Map.put(:quantity, number |> to_string)
-    |> Map.put(:container, "bottles")
-    |> Map.put(:successor, number - 1)
-    |> Map.put(:action, "Take one down and pass it around")
+    %{
+      struct
+      | quantity: to_string(number),
+        container: "bottles",
+        successor: number - 1,
+        action: "Take one down and pass it around"
+    }
   end
 
   def apply_overrides(%__MODULE__{number: 0} = struct) do
-    struct
-    |> Map.put(:quantity, "no more")
-    |> Map.put(:successor, 99)
-    |> Map.put(:action, "Go to the store and buy some more")
+    %{struct | quantity: "no more", successor: 99, action: "Go to the store and buy some more"}
   end
 
   def apply_overrides(%__MODULE__{number: 1} = struct) do
-    struct
-    |> Map.put(:container, "bottle")
-    |> Map.put(:action, "Take it down and pass it around")
+    %{struct | container: "bottle", action: "Take it down and pass it around"}
   end
 
   def apply_overrides(%__MODULE__{number: 6} = struct) do
-    struct
-    |> Map.put(:quantity, "1")
-    |> Map.put(:container, "six-pack")
+    %{struct | quantity: "1", container: "six-pack"}
   end
 
   def apply_overrides(struct), do: struct
@@ -56,13 +51,11 @@ defmodule Bottles do
     end
 
     def load_current_bottle(%__MODULE__{verse_number: verse_number, adapter: adapter} = struct) do
-      struct
-      |> Map.put(:current_bottle, adapter.new(verse_number))
+      %{struct | current_bottle: adapter.new(verse_number)}
     end
 
     def load_next_bottle(%__MODULE__{current_bottle: current_bottle, adapter: adapter} = struct) do
-      struct
-      |> Map.put(:next_bottle, adapter.successor(current_bottle))
+      %{struct | next_bottle: adapter.successor(current_bottle)}
     end
 
     def to_string(%__MODULE__{current_bottle: current_bottle, next_bottle: next_bottle}) do
