225da93f1ceaa47a2b8d922f2ddbe3afcc24e732
Steven Nunez <octosteve@github.com>
2020-12-11 12:11:57 -0500

    Give things some room

--- lib/bottles.ex
+++ lib/bottles.ex
@@ -7,32 +7,15 @@ defmodule BottleNumber do
     end
   end
 
-  def new(0) do
-    compose([&base_bottle_number/1, &bottle_0/1]).(0)
+  def new(number) do
+    struct!(__MODULE__, number: number)
+    |> apply_defaults
+    |> apply_overrides
   end
 
-  def new(1) do
-    compose([&base_bottle_number/1, &bottle_1/1]).(1)
-  end
-
-  def new(6) do
-    compose([&base_bottle_number/1, &bottle_6/1]).(6)
-  end
-
-  def new(bottle_number), do: compose([&base_bottle_number/1]).(bottle_number)
   def successor(%__MODULE__{successor: successor}), do: new(successor)
 
-  def compose(bottle_builders) do
-    fn number ->
-      bottle_builders
-      |> Enum.reduce(__struct__(number: number), fn builder, struct ->
-        struct
-        |> builder.()
-      end)
-    end
-  end
-
-  def base_bottle_number(%__MODULE__{number: number} = struct) do
+  def apply_defaults(%__MODULE__{number: number} = struct) do
     struct
     |> Map.put(:quantity, number |> to_string)
     |> Map.put(:container, "bottles")
@@ -40,35 +23,60 @@ defmodule BottleNumber do
     |> Map.put(:action, "Take one down and pass it around")
   end
 
-  def bottle_0(%__MODULE__{} = struct) do
+  def apply_overrides(%__MODULE__{number: 0} = struct) do
     struct
     |> Map.put(:quantity, "no more")
     |> Map.put(:successor, 99)
     |> Map.put(:action, "Go to the store and buy some more")
   end
 
-  def bottle_1(%__MODULE__{} = struct) do
+  def apply_overrides(%__MODULE__{number: 1} = struct) do
     struct
     |> Map.put(:container, "bottle")
     |> Map.put(:action, "Take it down and pass it around")
   end
 
-  def bottle_6(%__MODULE__{} = struct) do
+  def apply_overrides(%__MODULE__{number: 6} = struct) do
     struct
     |> Map.put(:quantity, "1")
     |> Map.put(:container, "six-pack")
   end
+
+  def apply_overrides(struct), do: struct
 end
 
 defmodule Bottles do
-  def verse(number, bottle_number \\ BottleNumber) do
-    current_bottle = bottle_number.new(number)
-    next_bottle = bottle_number.successor(current_bottle)
+  defmodule Bottles.Verse do
+    defstruct ~w(verse_number current_bottle next_bottle adapter)a
 
-    String.capitalize("#{current_bottle} of beer on the wall, ") <>
-      "#{current_bottle} of beer.\n" <>
-      "#{current_bottle.action}, " <>
-      "#{next_bottle} of beer on the wall.\n"
+    def new(verse_number, adapter) do
+      struct!(__MODULE__, verse_number: verse_number, adapter: adapter)
+      |> load_current_bottle
+      |> load_next_bottle
+    end
+
+    def load_current_bottle(%__MODULE__{verse_number: verse_number, adapter: adapter} = struct) do
+      struct
+      |> Map.put(:current_bottle, adapter.new(verse_number))
+    end
+
+    def load_next_bottle(%__MODULE__{current_bottle: current_bottle, adapter: adapter} = struct) do
+      struct
+      |> Map.put(:next_bottle, adapter.successor(current_bottle))
+    end
+
+    def to_string(%__MODULE__{current_bottle: current_bottle, next_bottle: next_bottle}) do
+      String.capitalize("#{current_bottle} of beer on the wall, ") <>
+        "#{current_bottle} of beer.\n" <>
+        "#{current_bottle.action}, " <>
+        "#{next_bottle} of beer on the wall.\n"
+    end
+  end
+
+  def verse(number, adapter \\ BottleNumber) do
+    number
+    |> Bottles.Verse.new(adapter)
+    |> Bottles.Verse.to_string()
   end
 
   def verses(upper, lower) do
